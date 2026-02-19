28a1542000c8883e840bcdf26228cce04504d339
Danielbaroe98 <daniel.baro@outlook.com>
2026-02-19 20:12:53 +0100

    lagt til HTML struktur og noe CSS

---
+++ src/pages/bookings/bookings.ts
@@ -0,0 +1,16 @@
+const bookingForm = document.getElementById("booking-form");
+const bookingRoomId = document.getElementById("booking-room-id");
+const checkInDate = document.getElementById("check-in-date");
+const checkOutDate = document.getElementById("check-out-date");
+const message = document.getElementById("message");
+
+const roomName = document.getElementById("roomname");
+const roomInfo = document.getElementById("roominfo");
+const roomPrice = document.getElementById("roomprice")
+
+const bookingContainer = document.getElementById("booking-container");
+const bookingMessage = document.getElementById("bookingmessage");
+
+const editModal = document.getElementById("edit-modal");
+const editForm = document.Get
+
