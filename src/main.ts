6b7fa3dead9589d81fb05baa341b8de132787abc
Berfin Esmèe Anli Akyol <berfinanli@hotmail.com>
2026-02-23 08:30:21 +0100

    navigasjon

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-// import './pages/explore/explore';
+const nav = document.getElementById("mainNav");
+const button = document.querySelector(".hamburger-button");
+
+const toggleNav =() => {
+    if (!nav) return;
+    nav.classList.toggle("open");
+};
+
+if (button) {
+    button.addEventListener("click", toggleNav);
+}
