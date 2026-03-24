3104d5dcc0d9dd47d595cc6928e9aa03abd6e823
Nathaliaf7 <nathaliafr93@gmail.com>
2026-03-24 19:31:05 +0100

    star-rating-doneee

--- src/pages/room-details/roomDetails.ts
+++ src/pages/room-details/roomDetails.ts
@@ -2,9 +2,12 @@ const modal = document.getElementById ('review-modal') as HTMLElement;
 const writeBtn = document.getElementById('write-a-review-btn') as HTMLElement;
 
 writeBtn.addEventListener('click', () => {
+    console.log('write-a-review-btn clicked');
     modal.classList.add ('active')});
 
-modal.addEventListener('click', (e) => {if (e.target === modal)
+modal.addEventListener('click', (e) => {
+    console.log('modal closed', e.target);
+    if (e.target === modal)
     { modal.classList.remove ('active'); }});
 
 const stars = document.querySelectorAll<HTMLSpanElement>('.review-stars span');
@@ -17,3 +20,15 @@ stars.forEach((star) => {
         updateStars(selectedrating);
     });
 });
+
+function updateStars(rating:number) {
+    stars.forEach((star)=> {
+        const value= Number (star.dataset.value);
+        if (value <= rating) {
+            star.classList.add('active');
+        }
+        else{ star.classList.remove ('active');
+
+        }
+    })
+}
